b993c8d575a38de5656ae1b63070055563ce5347
噢 <a23878759@163.com>
2019-12-03 09:05:20 +0800

    Initial Commit

---
+++ VehicleManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VehicleManagement
+//
+//  Created by mac on 2019/12/3.
+//  Copyright © 2019 ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VehicleManagement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VehicleManagement
+//
+//  Created by mac on 2019/12/3.
+//  Copyright © 2019 ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
